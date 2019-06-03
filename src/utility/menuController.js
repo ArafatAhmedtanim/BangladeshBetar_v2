021458aa963f1f102bd2b95966fcbd3710a85e26
adnandiu31 <adnandiu31@gmail.com>
2019-06-03 13:05:46 +0600

    sider list add

--- src/utility/menuController.js
+++ src/utility/menuController.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './../pages/home/home';
+import Station from './../pages/Station/station';
+import Manufacturer from './../pages/Manufacturer/manufacturer';
+import Product from './../pages/Product/product';
+import Fault from './../pages/Fault/fault';
+import Indent from './../pages/Indent/indent';
+import SIB from './../pages/SIB/sib';
+import Ledger from './../pages/Ledger/ledger';
+import FAQ from './../pages/FAQ/faq';
 
 export default function getSelectedContent(selectedMenu) {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/station" component={Station} />
+      <Route exact path="/manufacturer" component={Manufacturer} />
+      <Route exact path="/product" component={Product} />
+      <Route exact path="/fault" component={Fault} />
+      <Route exact path="/indent" component={Indent} />
+      <Route exact path="/sib" component={SIB} />
+      <Route exact path="/ledger" component={Ledger} />
+      <Route exact path="/faq" component={FAQ} />
     </Switch>
   );
 }
--- src/utility/menuList.js
+++ src/utility/menuList.js
@@ -1,4 +1,14 @@
-export const MenuList = [{ name: 'Home', link: '/' }];
+export const MenuList = [
+  { name: 'Home', link: '/' },
+  { name: 'Station', link: '/station' },
+  { name: 'Manufacturer', link: '/manufacturer' },
+  { name: 'Product', link: '/product' },
+  { name: 'Fault', link: '/fault' },
+  { name: 'Indent', link: '/indent' },
+  { name: 'SIB', link: '/sib' },
+  { name: 'Ledger', link: '/Ledger' },
+  { name: 'FAQ', link: '/faq' },
+];
 
 export default {
   MenuList,
